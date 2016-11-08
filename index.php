8a2d98fc83f7e8ff9891e2fd934d602804b03906
rocjrocj <roc@rocjohnson.com>
2016-11-07 18:21:03 -0600

    zzz

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php require_once('header.php'); ?>
 <script src="https://use.fontawesome.com/7e4bf047e3.js"></script>
-
-
-		
-
-<div id="blog">
+<div id="blog"> 
 	<!-- mm blog header start -->
 	<div class="blog_hea">
 		<div class="container">
 			<div class="row">
 				<h1>Monday Morning</h1>
 				<h2>Barbara K. Rimer, DRPH</h2>
-			</div><!-- /.row -->
+			</div>
+			<!-- /.row -->
 			<div class="row">
 				<ul>
 					<li><a href="#">Introduction</a></li>
@@ -19,13 +16,13 @@
 					<li><a href="#">Archives</a></li>
 					<li><a href="#">Contact</a></li>
 				</ul>
-			</div><!-- /.row -->
-		</div><!-- /.container -->
+			</div>
+			<!-- /.row --> 
+		</div>
+		<!-- /.container --> 
 	</div>
-	<!-- mm blog header end -->
+	<!-- mm blog header end --> 
 </div>
-
-
 <div class="page_hero">
 	<div class="container">
 		<div class="hero_content">
@@ -33,54 +30,52 @@
 				<h5>Higher Ed</h5>
 				<h3>Freedom of Speech</h3>
 			</div>
-			<div class="hero_img">
-				<img class="img-responsive" src="images/hero.png" alt="" />
-			</div>
-		</div><!-- /.hero_content -->
-	</div><!-- /.container -->
-</div><!-- /.page_hero -->
-
+			<div class="hero_img"> <img class="img-responsive" src="images/hero.png" alt="" /> </div>
+		</div>
+		<!-- /.hero_content --> 
+	</div>
+	<!-- /.container --> 
+</div>
+<!-- /.page_hero -->
 
 <div id="main_content">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-9 page_contentz">
-			<div class="row clearfix">
-				<img src="images/pho.png" class="pull-left" alt=""/>
-				<h6>Government, leadership, Technology</h6>
-				<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
-				<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
-			</div>
-			<div class="row clearfix">
-				<img src="images/pho.png" class="pull-left" alt=""/>
-				<h6>Government, leadership, Technology</h6>
-				<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
-				<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
-			</div>
-			<div class="row clearfix">
-				<img src="images/pho.png" class="pull-left" alt=""/>
-				<h6>Government, leadership, Technology</h6>
-				<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
-				<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
-			</div>
-			<div class="pagination-centered">
-						<ul class="pagination">
-							<li class='current'><a class="noclick" href='#'>1</a></li>
-							<li class="bar"></li>
-							<li><a class='' href='http://mondaymorning.web.unc.edu/page/2/'>2</a></li>
-							<li class="bar"></li>
-							<li><a class='' href='http://mondaymorning.web.unc.edu/page/3/'>3</a></li>
-							<li class="bar"></li>
-							<li><a class='' href='http://mondaymorning.web.unc.edu/page/4/'>4</a></li>
-							<li class="bar"></li>
-							<li><a class='' href='http://mondaymorning.web.unc.edu/page/5/'>5</a></li>
-							<li class="bar"></li>
-							<li><a class="next" href="http://mondaymorning.web.unc.edu/page/2/">Older ></a></li>
-						</ul>
-					</div>
-			<!--// end .pagination -->
+				<div class="row clearfix"> <img src="images/pho.png" class="pull-left" alt=""/>
+					<h6>Government, leadership, Technology</h6>
+					<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
+					<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
+				</div>
+				<div class="row clearfix"> <img src="images/pho.png" class="pull-left" alt=""/>
+					<h6>Government, leadership, Technology</h6>
+					<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
+					<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
+				</div>
+				<div class="row clearfix"> <img src="images/pho.png" class="pull-left" alt=""/>
+					<h6>Government, leadership, Technology</h6>
+					<h3>President Obama looks with optimism to a future that’s better for everybody</h3>
+					<p>Why science is key I haven’t watched television in more than two years. The brief snippets I get at the gym are a potent reminder that a constant dose of television news could leave one feeling down about the state of the country and the world. (Of course, that statement… <a href="#"><span class="glyphicon glyphicon-arrow-right"></span></a></p>
+				</div>
+				<div class="pagination-centered">
+					<ul class="pagination">
+						<li class='current'><a class="noclick" href='#'>1</a></li>
+						<li class="bar"></li>
+						<li><a class='' href='http://mondaymorning.web.unc.edu/page/2/'>2</a></li>
+						<li class="bar"></li>
+						<li><a class='' href='http://mondaymorning.web.unc.edu/page/3/'>3</a></li>
+						<li class="bar"></li>
+						<li><a class='' href='http://mondaymorning.web.unc.edu/page/4/'>4</a></li>
+						<li class="bar"></li>
+						<li><a class='' href='http://mondaymorning.web.unc.edu/page/5/'>5</a></li>
+						<li class="bar"></li>
+						<li><a class="next" href="http://mondaymorning.web.unc.edu/page/2/">Older ></a></li>
+					</ul>
+				</div>
+				<!--// end .pagination --> 
 			</div>
 			<div class="col-md-3 page_contentz">
+				<div class="blogroll">
 					<h3>Blogroll</h3>
 					<ul class="">
 						<li><a href="http://www.asph.org/">ASPH</a></li>
@@ -96,13 +91,13 @@
 						<li><a href="http://www.walt.allthingsd.com/">Walter Mossberg</a></li>
 					</ul>
 				</div>
-			
+				<div class="rss">
+					RSS Feed 
+					Get the latest blog updates from
+					Monday Morning
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
-		
-		
-
-
 <?php require_once('footer.php'); ?>
-
